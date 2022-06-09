526f6f38ed19184d6b2531ec14a3146fdadaaf9f
lxj6 <284443490@qq.com>
2022-06-09 23:55:37 +0800

    add field to user

---
+++ database/migrations/2022_06_09_235425_add_field_to_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFieldToUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user', function (Blueprint $table) {
+            $table->string('head_portrait',255)->after('user_name')->default('')->comment('头像');
+            $table->string('autograph',255)->after('head_portrait')->default('')->comment('签名');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user', function (Blueprint $table) {
+            $table->dropColumn('head_portrait');
+            $table->dropColumn('autograph');
+        });
+    }
+}
