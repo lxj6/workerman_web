b17570ecd05ecc34e9ba24b45690ba4dfd37938f
lxj6 <284443490@qq.com>
2022-06-25 22:21:09 +0800

    testController

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -11,9 +11,6 @@ class TestController extends Controller
 {
     public function test()
     {
-        $arr = ['test' => 1,'test2' => 233];
-        echo now()->addSeconds(1);
-        $bool = MyTestJob::dispatch($arr)->onQueue('MyTestJob');
-        var_dump($bool);
+
     }
 }
